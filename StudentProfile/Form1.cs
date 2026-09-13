aeec2dc5bb80299331412b48ccf3a666d7ae4f9c
Ariello <Ariello@DESKTOP-SMD1DHH>
2026-09-13 16:14:21 +0800

    Add StudentProfile Visual Studio project.

---
+++ StudentProfile/Form1.cs
@@ -0,0 +1,11 @@
+namespace StudentProfile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            MessageBox.Show("Student Profile - Juan Dela Cruz");
+        }
+    }
+}
